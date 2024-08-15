ada7ce888d6a596d191e98f276e849255b1a5eaa
vidit mathur <viditganpi10@gmail.com>
2024-08-15 14:27:42 +0530

    Making image scaling better

--- src/components/RecipieCard.tsx
+++ src/components/RecipieCard.tsx
@@ -10,9 +10,9 @@ const RecipieCard: React.FC<{recipieDetails: RecipeItem}> = (props) => {
     
     return (
         <div className="w-1/3 md:w-1/6 p-4 m-4 bg-white rounded-lg shadow-lg shadow-slate-400 hover:cursor-pointer hover:shadow-slate-800" onClick={handleCardClick}>
-            <img src={recipieDetail.image} alt="recipe" className="h-36 md:h-58 w-full"></img>
+            <img src={recipieDetail.image} alt="recipe" className="w-full object-cover rounded-t-lg"></img>
             <div>
-                <h1 className="text-base md:text-3xl">{recipieDetail.name}</h1>
+                <h1 className="text-base md:text-2xl">{recipieDetail.name}</h1>
             </div>
         </div>
     );
